a93fb0e0f9cf042628cb83e2f6f84dd63f25c048
Daniel Szulc <daniel.szulc198@gmail.com>
2025-01-28 16:41:03 +0100

    1.1.1 hotfix

--- src/lib/components/WeatherWidget.js
+++ src/lib/components/WeatherWidget.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/styles.scss';
+import './styles/styles.css';
 import openWeather from './providers/openWeather.js';
 import {useEffect, useState} from "react";
 import {weatherIcon} from "./icons/weather-icons.js";
